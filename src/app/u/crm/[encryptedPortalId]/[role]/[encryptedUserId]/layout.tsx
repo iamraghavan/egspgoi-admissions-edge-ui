eb353120b08a68a3bd2d28c9fb95a15e3fcfcbf1
1434_Raghavan <raghavanofficials@gmail.com>
2025-12-10 16:55:02 +0000

    add scroll main and inside the main component and element only dont dist

--- src/app/u/crm/[encryptedPortalId]/[role]/[encryptedUserId]/layout.tsx
+++ src/app/u/crm/[encryptedPortalId]/[role]/[encryptedUserId]/layout.tsx
@@ -6,10 +6,12 @@ export default function CrmLayout({ children }: { children: ReactNode }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <AppSidebar />
-      <div className="flex flex-col">
+      <div className="flex flex-col h-screen overflow-hidden">
         <AppHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40">
-          {children}
+        <main className="flex-1 overflow-auto p-4 lg:p-6 bg-muted/40">
+          <div className="flex flex-col gap-4 lg:gap-6">
+            {children}
+          </div>
         </main>
       </div>
     </div>
