0048e389d99fc6600bcc77d758a3ecbfc32a131c
1434_Raghavan <raghavanofficials@gmail.com>
2026-01-08 16:48:49 +0000

    ADD A NEW FEATURE
    To make your website load and work offline when there

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ const spaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
   title: 'Admissions Edge',
   description: 'Streamlining admissions with cutting-edge tools.',
+  manifest: '/manifest.json'
 };
 
 export default function RootLayout({
