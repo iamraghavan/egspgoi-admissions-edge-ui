c1e2d042f23c65bb6b001499e2d6675b2ae693bb
1434_Raghavan <raghavanofficials@gmail.com>
2026-01-11 02:47:58 +0000

    remove continue with google just only in login  (_for element div_)

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -131,19 +132,6 @@ export function LoginForm() {
         <Button type="submit" className="w-full" disabled={isLoading}>
           {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
           Login
-        </Button>
-        <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-                </span>
-            </div>
-        </div>
-        <Button variant="outline" className="w-full" type="button">
-          Google
         </Button>
       </form>
     </Form>
